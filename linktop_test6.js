64eb88c1c5aa6a7b28ff3c075dcf8612d6952f5e
khoadd101 <khoadd101@yahoo.com>
2024-09-17 22:54:54 +0700

    Add files via upload

---
+++ linktop_test6.js
@@ -0,0 +1,25 @@
+function LinktopBoxlink(_element)
+{
+  const element = document.body.querySelector(_element);
+  
+  document.addEventListener('DOMContentLoaded', () => {
+    const url = 'https://textlink.linktop.vn/?adslk=';
+    //url += btoa(window.location.href) + '&dmn=' + btoa(window.location.hostname);
+	url += btoa('http://antt.vn/')+'&dmn='+btoa('antt.vn');
+	console.log('Url: ', url);
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200)
+	  {
+        try
+		{
+          element.insertAdjacentHTML("beforebegin", this.responseText);
+        } catch (error) {
+          console.error("Error inserting content:", error);
+        }
+      }
+    };
+    xhr.open("GET", url);
+    xhr.send();
+  });
+}
